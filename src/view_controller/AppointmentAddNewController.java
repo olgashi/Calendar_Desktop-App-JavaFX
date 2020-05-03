3a47a2a3c2326339a4fcf3cb29ba59ef4921418d
Olga Shiryaeva <oshiryaeva@protonmail.com>
2020-05-02 19:13:16 -0500

    added input validation to AppointmentAddNewController

--- src/view_controller/AppointmentAddNewController.java
+++ src/view_controller/AppointmentAddNewController.java
@@ -1,16 +1,20 @@
 package view_controller;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+import model.Customer;
+import utilities.AlertMessage;
 import utilities.dbQuery;
+
+import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import static javafx.collections.FXCollections.observableArrayList;
@@ -36,7 +40,15 @@ public class AppointmentAddNewController implements Initializable {
     @FXML
     private DatePicker newAppointmentDatePicker;
     @FXML
-    private TextField newAppointmentTimeTextField;
+    private RadioButton timeAM;
+    @FXML
+    private RadioButton timePM;
+    @FXML
+    private ToggleGroup amPMtoggleGroup;
+    @FXML
+    private TextField newAppointmentTimeHoursTextField;
+    @FXML
+    private TextField newAppointmentTimeMinutesTextField;
     @FXML
     private TextField newAppointmentTypeTextField;
     @FXML
@@ -44,44 +56,88 @@ public class AppointmentAddNewController implements Initializable {
     @FXML
     private TextField newAppointmentTitleTextField;
     @FXML
-    private TableView<ObservableList<String>>customerTable;
+    private TextField newAppointmentLocationTextField;
     @FXML
-    private TableColumn<String,String> appointmentCustomerNameColumn;
+    private TableView<Customer> appointmentCustomerTable;
     @FXML
-    private TableView<String> locationTable;
+    private TableColumn<Customer,String> appointmentCustomerNameColumn;
     @FXML
-    private TableColumn<String,String> appointmentLocationColumn;
+    private TableColumn<Customer,String> appointmentCustomerLocationColumn;
+    @FXML
+    private TableColumn<Customer,String> appointmentCustomerPhoneNumberColumn;
     @FXML
     private Button addAppointmentCancelButton;
     @FXML
     private Button addAppointmentCreateButton;
 
-    private ObservableList<String> locationList = observableArrayList();
 
+    private boolean validateTime(){
+        String regexDigits = "[0-9]+";
+        String tHours = newAppointmentTimeHoursTextField.getText();
+        String tMinutes = newAppointmentTimeMinutesTextField.getText();
 
-// Populate Customer table, Location table on window load
-    public void getDataForLocationTable() throws SQLException {
+        if ((tHours.length() < 3 && tMinutes.length() < 3) && (tHours.matches(regexDigits) && tMinutes.matches(regexDigits))) return true;
+        else return false;
+    }
 
-        dbQuery.createQuery("SELECT DISTINCT location FROM appointment");
-        while(dbQuery.getQueryResultSet().next()){
-            System.out.println(dbQuery.getQueryResultSet().getString("location"));
-//            locationList.add(dbQuery.getQueryResultSet().getString("location"));
-//            System.out.println(locationList.size());
-            locationList.add(dbQuery.getQueryResultSet().getString("location"));
+    public boolean validateEmptyInputsAddNewAppointment() {
+        String aTitle,aDate, aTimeHours, aTimeMinutes, aLocation, aType, aDescription;
+        try {
+            aTitle = newAppointmentTitleTextField.getText();
+            aDate = newAppointmentDateText.getText();
+            aTimeHours = newAppointmentTimeHoursTextField.getText();
+            aTimeMinutes = newAppointmentTimeMinutesTextField.getText();
+            aLocation = newAppointmentLocationTextField.getText();
+            aType = newAppointmentTypeTextField.getText();
+            aDescription = newAppointmentDescriptionTextField.getText();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
+        if (aTitle.isEmpty() || aDate.isEmpty() || aTimeHours.isEmpty() || aTimeMinutes.isEmpty() ||
+                aLocation.isEmpty() || aType.isEmpty() || aDescription.isEmpty()) return false;
+        else return true;
+    }
+
+    private void loadCustomerTableData (){
+        Customer.clearCustomerList();
+        dbQuery.createQuery("SELECT customerId, customerName, address, city, postalCode, country, phone FROM customer, address, city, country " +
+                "WHERE customer.addressId = address.addressId AND address.cityId = city.cityId AND city.countryId = country.countryId");
+        ResultSet rs = dbQuery.getQueryResultSet();
+        try {
+            while(dbQuery.getQueryResultSet().next()) {
+                Customer.getCustomerList().add(new Customer(rs.getString("customerId"), rs.getString("customerName"),
+                        rs.getString("address"), rs.getString("city"), rs.getString("postalCode"),
+                        rs.getString("country"), rs.getString("phone")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
+    public void createAppointment(ActionEvent event) throws SQLException, IOException {
+
+        Customer selectedCustomer = appointmentCustomerTable.getSelectionModel().getSelectedItem();
+        if (!validateEmptyInputsAddNewAppointment() || !validateTime() || selectedCustomer == null) {
+            AlertMessage.display("Inputs are invalid. Please check and try again.", "warning");
+        } else {
+            // create appointment
+        }
+    }
 
 //Table is not populating
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            getDataForLocationTable();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        appointmentLocationColumn.setCellValueFactory(new PropertyValueFactory<>(""));
-        locationTable.setItems(locationList);
+        amPMtoggleGroup = new ToggleGroup();
+        this.timeAM.setToggleGroup(amPMtoggleGroup);
+        this.timePM.setToggleGroup(amPMtoggleGroup);
+        timeAM.setSelected(true);
+
+        loadCustomerTableData();
+        appointmentCustomerNameColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("customerName"));
+        appointmentCustomerLocationColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("customerCity"));
+        appointmentCustomerPhoneNumberColumn.setCellValueFactory(new PropertyValueFactory<Customer,String>("customerPhoneNumber"));
+        appointmentCustomerTable.setItems(Customer.getCustomerList());
     }
 }
 // TODO include info popups on hover for input fields
