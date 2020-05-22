48b84bd634a9d692ed421a07846999cbf04d0245
Olga Shiryaeva <oshiryaeva@protonmail.com>
2020-05-22 12:55:46 -0500

    Removed empty lines

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import utilities.DBConnection;
-
 import java.sql.SQLException;
 
 public class Main extends Application {
@@ -17,7 +16,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) throws SQLException {
         DBConnection.startConnection();
